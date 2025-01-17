30af4faf5b70b37c56aa08910cc4d62647e36c38
Aman Varshney <av.amanvarshney11@gmail.com>
2025-01-17 19:47:24 +0530

    update deps

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -5,9 +5,9 @@ import { DocsLayout } from "fumadocs-ui/layouts/docs";
 import type { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
-	return (
-		<DocsLayout tree={source.pageTree} {...baseOptions}>
-			{children}
-		</DocsLayout>
-	);
+  return (
+    <DocsLayout tree={source.pageTree} {...baseOptions}>
+      {children}
+    </DocsLayout>
+  );
 }
